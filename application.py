70f5b4f528aa121891e67492d8cec3585520599e
minimac1 <mini.mac@live.com>
2018-04-16 19:29:24 +1000

    Documented new changes, slightly changed page organisation and added gui page(not done yet)

--- application.py
+++ application.py
@@ -7,18 +7,27 @@ application = Flask(__name__)
 application.register_blueprint(api_v1, url_prefix='/newsapi/v1.0')
 application.register_blueprint(api_v2, url_prefix='/newsapi/v2.0')
 
-@application.route('/newsapi')
-@application.route('/newsapi/homepage')
+@application.route('/')
 ##@app.route('/News/<name>')
 def base():
     return render_template('homepage.html')
 
 def apiIndex():
     return render_template('homepage.html')
+    
+@application.route('/newsapi')
+def apiHome():
+    return render_template('apiHome.html')
+
+@application.route('/newsapi/gui')
+def gui():
+    return render_template('gui.html')
+
+
 
 # change homepage to gui implementation
-def gui():
-    return render_template('homepage.html')
+#def gui():
+#    return render_template('homepage.html')
 
 @application.route('/newsapi/changes')
 def changesPage():
@@ -31,6 +40,7 @@ def featuresPage():
 @application.route('/newsapi/test')
 def testPage():
     return render_template('test.html')
+    
 
 #Temporary
 @application.route('/homepage')
@@ -48,9 +58,6 @@ def featuresPage1():
 @application.route('/test')
 def testPage1():
     return render_template('test.html')
-
-
-
 
 
 # this will change with a gui
