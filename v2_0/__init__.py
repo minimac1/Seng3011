e1d525947db89c4197822119c93bedf1d4e61c11
Unknown <rohanbhat@hotmail.com>
2018-04-20 21:43:54 +1000

    Bug Fix
    Bug Fix

--- v2_0/__init__.py
+++ v2_0/__init__.py
@@ -383,6 +383,7 @@ class InputProcess(Resource):
                 return errorReturn(4, args)
 
         for idx, val in enumerate(compId):
+            compId[idx] = asxNameToCode(val)
             abbrevID.append(removeExchangeCode(asxNameToCodeFuzzy(asxCodeToName(val))))
 
 
