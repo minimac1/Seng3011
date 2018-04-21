c19cdbf8aa80ac4f25e6193c19b64edd283f8da2
Dominic Mcdonald <8554453+minimac1@users.noreply.github.com>
2018-04-21 15:14:08 +1000

    Update __init__.py

--- v2_0/__init__.py
+++ v2_0/__init__.py
@@ -398,10 +398,10 @@ class InputProcess(Resource):
         for c in comp:
             c = c.rstrip('-')
             c = c.lstrip('-')
-            if re.search("[^\.\-\w]",c) is not None or c.count('.')>1 or "--" in c or c is "":
-                return errorReturn(12,args)
             if c is "":
                 return errorReturn(14,args)
+            if re.search("[^\.\-\w]",c) is not None or c.count('.')>1 or "--" in c:
+                return errorReturn(12,args)            
             a = c.replace("-", " ")
             b = a.upper()
             compCheck.append(b)
@@ -447,10 +447,10 @@ class InputProcess(Resource):
         for c in topics:
             c = c.rstrip('-')
             c = c.lstrip('-')
-            if re.search("[^\"\-\w]",c) is not None or c.count('\"')==1 or c.count('\"') > 2 or c is "\"\"" or "--" in c:
-                return errorReturn(13,args)
             if c is "":
                 return errorReturn(15,args)
+            if re.search("[^\"\-\w]",c) is not None or c.count('\"')==1 or c.count('\"') > 2 or c is "\"\"" or "--" in c:
+                return errorReturn(13,args)
             a = c.replace("-", "%20")
             topicTemp.append(a)
 
